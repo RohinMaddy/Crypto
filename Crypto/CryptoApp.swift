72a69ccb1babb0cccc589c2cc2403aa6963efb92
Rohin Madhavan <rohinmadhavan96@gmail.com>
2024-06-14 12:23:26 +0100

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Rohin Madhavan on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
